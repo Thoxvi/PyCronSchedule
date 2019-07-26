aeda1ba7da0a13198d472fb3cc8eec18793abb7b
Thoxvi <A@Thoxvi.com>
2019-07-27 02:35:42 +0800

    困了

--- py_cron_schedule/cron_schedule.py
+++ py_cron_schedule/cron_schedule.py
@@ -8,6 +8,7 @@ from typing import AnyStr, Callable
 
 __all__ = [
   "CronSchedule",
+  "CronFormatError"
 ]
 
 logger = logging.getLogger("CronSchedule")
@@ -50,7 +51,7 @@ class CronSchedule(object):
   def start(self,
             hook_in_start: Callable = None,
             hook_in_end: Callable = None,
-            min_schedule_ms=1000) -> None:
+            min_schedule_ms=0.5) -> None:
     if not self.__task_dict:
       logger.warning("Task queue is empty.")
       return
@@ -75,9 +76,27 @@ class CronSchedule(object):
 
 
 class CronTimer(object):
+  # (min, max, millisecond)
+  TIME_RANGE = [
+    (1, 7, 1000 * 60 * 60 * 24 * 7),
+    (1, 12, 1000 * 60 * 60 * 24 * 30),
+    (1, 30, 1000 * 60 * 60 * 24),
+    (0, 23, 1000 * 60 * 60),
+    (0, 59, 1000 * 60),
+    (0, 59, 1000),  # second
+    (0, 99, 1),  # millisecond
+  ]
+
   def __calculate_next_time(self) -> float:
-    # TODO 根据算法计算出下一次执行的时间
-    pass
+    next_time = time.time()
+
+    for cron_unit_parts_index in range(len(self.__cron_data)):
+      cron_unit_parts = self.__cron_data[cron_unit_parts_index]
+      if len(cron_unit_parts) == 2:
+        num = int(cron_unit_parts[1])
+        next_time += num * CronTimer.TIME_RANGE[cron_unit_parts_index][2]
+
+    return next_time
 
   def __init__(self,
                cron_format: AnyStr):
@@ -85,7 +104,53 @@ class CronTimer(object):
     计算最小单位：微秒(以浮点形式，例如：1564157752.9863145)
     [毫秒] [秒] 分 时 日 月 周
     '''
-    # TODO 解析 cron_format，并生成计算方式供计算使用
+    self.__cron_data = []
+
+    cron_format = cron_format.strip()
+    cron_list = re.split("\s+", cron_format)[::-1]
+
+    if len(cron_list) < 5 or len(cron_list) > 7:
+      raise CronFormatError(
+        "The number of [" + cron_format + "] format parameters is incorrect: " + str(len(cron_list)))
+
+    for cron_unit_index in range(len(cron_list)):
+      cron_unit = cron_list[cron_unit_index]
+      std_range = CronTimer.TIME_RANGE[cron_unit_index]
+
+      # */5 or 5 or *
+      if re.search(r"[^0-9*/]", cron_unit) is None:
+        cron_unit_parts = cron_unit.split("/")
+        if len(cron_unit_parts) > 2:
+          raise CronFormatError("[" + cron_unit + "] format error")
+        elif len(cron_unit_parts) == 2:
+          if (cron_unit_parts[0] != "*" or
+              len(re.findall("\d+", cron_unit)) != 1):
+            raise CronFormatError("[" + cron_unit + "] format error")
+          else:
+            self.__cron_data.append(["every"] + cron_unit_parts)
+            continue
+        elif len(cron_unit_parts) == 1:
+          if cron_unit_parts[0] == "*":
+            self.__cron_data.append(["star"] + cron_unit_parts)
+            continue
+          else:
+            if re.match("\d+$", cron_unit_parts[0]) is not None:
+              num = int(cron_unit_parts[0])
+              if num < std_range[0] or num > std_range[1]:
+                raise CronFormatError("[" + str(num) + "] out of range")
+              else:
+                self.__cron_data.append(["number"] + cron_unit_parts)
+                continue
+        # 1-4
+        elif re.search(r"[^0-9\-]", cron_unit) is None:
+          pass
+        # 1,2,3
+        elif re.search(r"[^0-9,]", cron_unit) is None:
+          pass
+        else:
+          raise CronFormatError("[" + str(cron_unit_parts) + "] format error")
+
+        self.__cron_data.append(cron_unit_parts)
 
     self.__next_time = self.__calculate_next_time()
 
@@ -95,3 +160,7 @@ class CronTimer(object):
       return True
     else:
       return False
+
+
+class CronFormatError(Exception):
+  pass
